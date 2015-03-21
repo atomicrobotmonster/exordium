0335b2318ec226540d4128aa5acd3143776d19f5
Andy Hull <andy@atomicrobotmonster.com>
2015-03-21 13:06:30 -0700

    tests for attribute validator

--- chargen/tests.py
+++ chargen/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from models import validate_attribute
+from django.core.exceptions import ValidationError
 
-# Create your tests here.
+class AttributeValidatorTests(TestCase):
+    """
+    attribute validator should only permit values of -1 to 5 inclusive.
+    """
+
+    def test_validate_attribute_below_range(self):
+        with self.assertRaises(ValidationError):
+            validate_attribute(-2)
+
+    def test_attribute_above_range(self):
+        with self.assertRaises(ValidationError):
+            validate_attribute(6)
+
+    def test_attribute_lower_threshold(self):
+        validate_attribute(-1)
+
+    def test_attribute_upper_threshold(self):
+        validate_attribute(5)

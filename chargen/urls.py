4afdbba39f762147a6882cb3c9020da4b9748dc0
Andy Hull <andy@atomicrobotmonster.com>
2015-03-21 17:23:58 -0700

    user page requires authorization

--- chargen/urls.py
+++ chargen/urls.py
@@ -4,6 +4,6 @@ from chargen import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^login', views.login_user, name='my-login'),
-    url(r'^user', views.show_user, name='user'),
+    url(r'^login$', views.login_user, name='my-login'),
+    url(r'^user/(\d+)$', views.show_user, name='user'),
 )
--- chargen/views.py
+++ chargen/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from django import forms
+from django.http import HttpResponseForbidden
+from models import UserProfile
 
 class LoginCredentialsForm(forms.Form):
     username = forms.CharField(max_length=100)
@@ -15,12 +18,11 @@ def login_user(request):
     form = LoginCredentialsForm(request.POST)
 
     if form.is_valid():
-        print form.cleaned_data
         user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
         if user is not None:
             if user.is_active:
                 login(request, user)
-                return redirect("/user")
+                return redirect("/user/%s" % user.id)
             else:
                 form.add_error(None, 'Account disabled.')
                 return render(request, "chargen/index.html", { 'form': form } )
@@ -31,5 +33,10 @@ def login_user(request):
         return render(request, "chargen/index.html", { 'form': form } )
 
 
-def show_user(request):
-    return render(request,"chargen/user.html")
+@login_required
+def show_user(request,user_id):
+    if request.user.id != int(user_id):
+        return HttpResponseForbidden()
+                
+    user_profile = get_object_or_404(UserProfile, user_id=user_id)
+    return render(request,"chargen/user.html", { 'userProfile': user_profile })

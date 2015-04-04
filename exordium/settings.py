814f2facfb58f3bba9499a8c0b25dc292faad05f
Andy Hull <andy@atomicrobotmonster.com>
2015-04-04 12:20:17 -0700

    Enabled Basic Auth

--- exordium/settings.py
+++ exordium/settings.py
@@ -87,5 +87,8 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+    )
 }

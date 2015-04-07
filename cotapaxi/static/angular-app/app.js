3e240167899c18472a03e8d5224aafdd97e7783e
Andy Hull <andy@atomicrobotmonster.com>
2015-04-07 14:51:20 -0700

    Refactored into multiple controllers

--- cotapaxi/static/angular-app/app.js
+++ cotapaxi/static/angular-app/app.js
@@ -5,13 +5,23 @@ cotopaxiApp.config(['$routeProvider',
     $routeProvider.
       when('/', {
         templateUrl: 'static/angular-app/views/main.html',
+        controller:  'CharacterEditorController'
       }).
-
+      when('/new-character', {
+        templateUrl: 'static/angular-app/views/main.html',
+        controller:  'NewCharacterController'
+      }).
+      when('/character/:characterId', {
+        templateUrl: 'static/angular-app/views/main.html',
+        controller:  'CharacterEditorController'
+      }).
       when('/sign-up', {
         templateUrl: 'static/angular-app/views/signup.html',
+        controller:  'RegistrationController'
       }).
       when('/login', {
-        templateUrl: 'static/angular-app/views/login.html'
+        templateUrl: 'static/angular-app/views/login.html',
+        controller:  'LoginController'
       }).
       otherwise({
         redirectTo: '/'
--- cotapaxi/static/angular-app/controllers/userController.js
+++ cotapaxi/static/angular-app/controllers/userController.js
@@ -14,12 +14,79 @@ cotopaxiUser.factory('Character', function($resource) {
   }) 
 })
 
-cotopaxiUser.controller('UserController', function ($http, $location, $scope, Registration, UserProfile, Character) {
-  $scope.$on('$routeChangeSuccess', function () {
-    $scope.badCredentials = false
-    $scope.authentication.password = ''
-  });
+cotopaxiUser.controller('AppController', function ($http, $location, $scope) {
+  $scope.isActive = function (viewLocation) {
+    var active = (viewLocation === $location.path());
+    return active;
+  }
 
+  $scope.logout = function() {
+    $scope.shared.authenticated = false
+    $scope.shared.userProfile = null
+    delete $http.defaults.headers.common['Authorization']
+
+    $location.path('/')
+  }
+
+  $scope.shared = { userProfile: null, authenticated: false}
+})
+
+cotopaxiUser.controller('LoginController', function ($http, $location, $scope, UserProfile) {
+
+  function handleHttpError(error) {
+    if (error.status == 401) {    
+      $scope.badCredentials = true
+      $scope.authentication.password = ''
+    } else {
+      console.log("The API returned an error: status [" + error.status + "] statusText [" + error.statusText + "]")
+    }
+  }
+
+  $scope.login = function() {
+    /* FIXME crude, will apply to all HTTP requests */
+    $http.defaults.headers.common['Authorization'] = 'Basic ' + btoa($scope.authentication.username + ':' + $scope.authentication.password)
+    
+    $scope.shared.userProfile = UserProfile.get(function(data) {
+      $scope.shared.authenticated = true
+      $location.path('/')
+    }, handleHttpError)
+  }
+
+  $scope.badCredentials = false;
+  $scope.shared.authenticated = false
+})
+
+cotopaxiUser.controller('RegistrationController', function ($http, $location, $scope, Registration, UserProfile) {
+
+  function handleHttpError(error) {
+    if (error.status == 401) {    
+      $scope.badCredentials = true
+      $scope.authentication.password = ''
+    } else {
+      console.log("The API returned an error: status [" + error.status + "] statusText [" + error.statusText + "]")
+    }
+  }
+
+  $scope.submitRegistration = function() {
+    $scope.registration.$save(function(data) {
+      $http.defaults.headers.common['Authorization'] = 'Basic ' + btoa($scope.registration.username + ':' + $scope.registration.password)
+      
+      $scope.shared.userProfile = UserProfile.get(function(data) {
+        $scope.shared.authenticated = true
+        $location.path('/')        
+      })
+    })
+  }
+
+  $scope.registration = new Registration
+  $scope.registration.username = ''
+  $scope.registration.first_name = ''
+  $scope.registration.last_name = ''
+  $scope.registration.email = ''
+  $scope.registration.password = ''
+})
+
+cotopaxiUser.controller('CharacterEditorController', function ($http, $location, $routeParams, $scope, UserProfile, Character) {
   $scope.isActive = function (viewLocation) {
     var active = (viewLocation === $location.path());
     return active;
@@ -38,54 +105,71 @@ cotopaxiUser.controller('UserController', function ($http, $location, $scope, Re
     }
   }
 
-  function refreshUserProfile(preferredIdInList) {
-    $scope.userProfile = UserProfile.get(function(data) {
-      $scope.authenticated = true
-      $scope.badCredentials = false
-
-      if (data.characters.length == 0) {
-        $scope.newCharacter()
-      } else {
-        var idToSelect
-        if (preferredIdInList) {
-          idToSelect = preferredIdInList
-        } else {
-          idToSelect = data.characters.sort(function(a, b) {
-            var x = a['name']; var y = b['name']
-            return ((x < y) ? -1 : ((x > y) ? 1 : 0))
-          })[0].id
-        }
-        $scope.selectCharacter(idToSelect)
-      }
-      $location.path('/')
-    }, handleHttpError)
+  function totalCharacterPoints(character) {
+    return character.unassigned_attribute_points 
+      + character.strength 
+      + character.agility
+      + character.mind
+      + character.appeal
   }
 
-  $scope.submitRegistration = function() {
-    $scope.registration.$save(function(data) {
-      $http.defaults.headers.common['Authorization'] = 'Basic ' + btoa($scope.registration.username + ':' + $scope.registration.password)
-      refreshUserProfile()
+  $scope.saveCharacter = function() {
+    if (totalCharacterPoints($scope.currentCharacter) != 4) {
+      $scope.badAttributePoints = true
+      return
+    } else {
+      $scope.badAttributePoints = false
+    }
+
+    $scope.currentCharacter.$update(function(data) {
+      $scope.shared.userProfile = UserProfile.get(function(userProfile) {
+        $location.path("/character/" + $scope.currentCharacter.id)
+      })
     })
-
   }
 
-  $scope.login = function() {
-    /* FIXME crude, will apply to all HTTP requests */
-    $http.defaults.headers.common['Authorization'] = 'Basic ' + btoa($scope.authentication.username + ':' + $scope.authentication.password)
-    refreshUserProfile()
+  $scope.deleteCharacter = function(id) {
+    $scope.currentCharacter.$remove({id: id}, function(data) {
+      $scope.shared.userProfile = UserProfile.get(function(userProfile) {
+        $location.path("/character")
+      })
+    })
   }
 
-  $scope.logout = function() {
-    $scope.authenticated = false
-    delete $http.defaults.headers.common['Authorization']
+  $scope.showNewCharacterLink = false
+  
+  var idToSelect = null
+  if ($scope.shared.userProfile) {  
+    if ($scope.shared.userProfile.characters.length == 0) {
+      $location.path('/new-character')
+      return
+    } else if ($routeParams.characterId) {
+      idToSelect = $routeParams.characterId
+    } else {
+      idToSelect = $scope.shared.userProfile.characters.sort(function(a, b) {
+        var x = a['name']; var y = b['name']
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0))
+      })[0].id
+    }
 
-    $location.path('/')
-  }
-
-  $scope.selectCharacter = function(characterId) {
     $scope.showNewCharacterLink = false
     $scope.badAttributePoints = false
-    $scope.currentCharacter = Character.get({ 'id': characterId })
+    $scope.currentCharacter = Character.get({ 'id': idToSelect })
+  }
+})
+
+cotopaxiUser.controller('NewCharacterController', function ($http, $location, $scope, UserProfile, Character) {
+  $scope.isEditing = function (characterId) {
+    return characterId === $scope.currentCharacter.id;
+  }
+
+  function handleHttpError(error) {
+    if (error.status == 401) {    
+      $scope.badCredentials = true
+      $scope.authentication.password = ''
+    } else {
+      console.log("The API returned an error: status [" + error.status + "] statusText [" + error.statusText + "]")
+    }
   }
 
   function totalCharacterPoints(character) {
@@ -104,45 +188,23 @@ cotopaxiUser.controller('UserController', function ($http, $location, $scope, Re
       $scope.badAttributePoints = false
     }
 
-    if (!$scope.currentCharacter.id) {
-      $scope.currentCharacter.$save(function(data) {
-        refreshUserProfile(data.id)
-      })      
-    } else {
-      $scope.currentCharacter.$update(function(data) {
-        refreshUserProfile($scope.currentCharacter.id)
+    $scope.currentCharacter.$save(function(savedCharacter) {
+      $scope.shared.userProfile = UserProfile.get(function(userProfile) {
+        $location.path("/character/" + savedCharacter.id)
       })
-    }
+    })      
   }
 
-  $scope.deleteCharacter = function(id) {
-    $scope.currentCharacter.$remove({id: id}, function(data) {
-      refreshUserProfile()
-    })
-  }
-
-  $scope.newCharacter = function() {
-    $scope.showNewCharacterLink = true
-    $scope.badAttributePoints = false
-    $scope.currentCharacter = new Character
-    $scope.currentCharacter.id = null
-    $scope.currentCharacter.name = 'Unknown Wanderer'
-    $scope.currentCharacter.unassigned_attribute_points = 4
-    $scope.currentCharacter.strength = 0
-    $scope.currentCharacter.agility = 0
-    $scope.currentCharacter.mind = 0
-    $scope.currentCharacter.appeal = 0
-    $scope.currentCharacter.user_profile = $scope.userProfile.id
-  }
-
-  $scope.showNewCharacterLink = false
-  $scope.badCredentials = false;
-  $scope.registration = new Registration
-  $scope.registration.username = ''
-  $scope.registration.first_name = ''
-  $scope.registration.last_name = ''
-  $scope.registration.email = ''
-  $scope.registration.password = ''
-  $scope.authenticated = false
-  $scope.authentication = { username: '', password: ''}
+  $scope.showNewCharacterLink = true
+  $scope.badAttributePoints = false
+  $scope.currentCharacter = new Character
+  $scope.currentCharacter.id = null
+  $scope.currentCharacter.name = 'Unknown Wanderer'
+  $scope.currentCharacter.unassigned_attribute_points = 4
+  $scope.currentCharacter.strength = 0
+  $scope.currentCharacter.agility = 0
+  $scope.currentCharacter.mind = 0
+  $scope.currentCharacter.appeal = 0
+  $scope.currentCharacter.user_profile = $scope.shared.userProfile.id
 })
+

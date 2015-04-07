a6d222558cd8c074facfed402b8ce6aedcd5a76c
Andy Hull <andy@atomicrobotmonster.com>
2015-04-07 10:29:26 -0700

    Added angular-app folder to improve organization

--- cotapaxi/static/angular-app/js/app.js
+++ cotapaxi/static/angular-app/js/app.js
@@ -4,14 +4,14 @@ cotopaxiApp.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
       when('/', {
-        templateUrl: 'static/partials/main.html',
+        templateUrl: 'static/angular-app/views/main.html',
       }).
 
       when('/sign-up', {
-        templateUrl: 'static/partials/signup.html',
+        templateUrl: 'static/angular-app/views/signup.html',
       }).
       when('/login', {
-        templateUrl: 'static/partials/login.html'
+        templateUrl: 'static/angular-app/views/login.html'
       }).
       otherwise({
         redirectTo: '/'

5921654d10f4c7ce3fa56e0472169ae68fab7187
Andy Hull <andy@atomicrobotmonster.com>
2015-04-12 11:08:15 -0700

    spinner-widget using ngModel

--- cotapaxi/static/angular-app/directives/spinnerWidget.js
+++ cotapaxi/static/angular-app/directives/spinnerWidget.js
@@ -3,13 +3,18 @@
 angular.module('cotopaxiApp').directive('spinnerWidget', function() {
 	return {
 	  restrict: 'E',
+	  require: 'ngModel',
 	  scope: {
-	    value: '@', 
         increment: '&',
         decrement: '&'
 	  },
       templateUrl: '/static/angular-app/views/spinnerWidget.html',
-      link: function(scope, elem, attrs) { 
+      link: function(scope, elem, attrs, ngModelController) { 
+
+      	 //render the value when the ng-model changes
+         ngModelController.$render = function() {
+           elem.find('span.spinner-widget-value').text(ngModelController.$viewValue)
+         }
       }
 	}
 })
